439c74723b4e2d2cb55d792380e5d24d46ff9158
Qiu, Han <Qiu.Han.Henry@vertivco.com>
2019-11-05 14:44:18 +0800

    refactor: refactor semaphore example
    refactor semaphore example

--- src/main/java/ConcurrentUtilsExample/SemaphoreExample.java
+++ src/main/java/ConcurrentUtilsExample/SemaphoreExample.java
@@ -1,9 +1,28 @@
 package ConcurrentUtilsExample;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
 public class SemaphoreExample {
-    public static void main(String[] args) {
-        Semaphore semaphore = new Semaphore(1);
+    public static void main(String[] args) throws InterruptedException {
+        Semaphore semaphore = new Semaphore(0);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        executorService.submit(()->{
+            System.out.println(Thread.currentThread().getName() + " over");
+            semaphore.release();
+        });
+
+        executorService.submit(()->{
+            System.out.println(Thread.currentThread().getName() + " over");
+            semaphore.release();
+        });
+
+        //等待子线程执行完毕，返回
+        semaphore.acquire(2);
+        System.out.println("all child threads over");
+        executorService.shutdown();
     }
 }
